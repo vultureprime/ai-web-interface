f547d23db5527e6edc2748a9f9961768baee7da9
ninew089 <132679550+nongnew52@users.noreply.github.com>
2023-11-11 21:57:57 +0700

    Update Alert.tsx

--- next-limit-api-interface/src/app/plan/components/Alert.tsx
+++ next-limit-api-interface/src/app/plan/components/Alert.tsx
@@ -126,7 +126,7 @@ export default function Alert({
                       <button
                         type='button'
                         className='inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:col-start-2'
-                        onClick={onSubmit}
+                        onClick={handleSubmit(onSubmit)}
                       >
                         Confirm
                       </button>
