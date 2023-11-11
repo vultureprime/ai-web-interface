bdb6b93a6fd8945b49cecac210d853ca5b9811c6
ninew089 <132679550+nongnew52@users.noreply.github.com>
2023-11-11 20:25:06 +0700

    init project

---
+++ next-limit-api-interface/src/app/page.tsx
@@ -0,0 +1,11 @@
+import PlanDetail from './plan/components/PlanDetail'
+import PlanHero from './plan/components/PlanHero'
+
+export default function Home() {
+  return (
+    <main>
+      <PlanHero />
+      <PlanDetail />
+    </main>
+  )
+}
