1730104b1316376da72f09461e59348e9ba7eed2
ninew089 <132679550+nongnew52@users.noreply.github.com>
2023-11-12 20:09:12 +0700

    feat: add word breake

--- next-limit-api-interface/src/app/plan/components/Alert.tsx
+++ next-limit-api-interface/src/app/plan/components/Alert.tsx
@@ -84,7 +84,7 @@ export default function Alert({
               leaveFrom='opacity-100 translate-y-0 sm:scale-100'
               leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
             >
-              <Dialog.Panel className='relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6'>
+              <Dialog.Panel className='relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all min-w-[300px] sm:my-8 sm:w-full sm:max-w-lg sm:p-6'>
                 <div>
                   {isSubmitSuccessful && (
                     <div className='mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100'>
@@ -129,7 +129,7 @@ export default function Alert({
                         </form>
                       ) : (
                         <div className='flex gap-x-2 my-8'>
-                          <p className='text-sm text-gray-500'>
+                          <p className='text-sm text-gray-500 break-all'>
                             API Key: {APIkey}
                           </p>
                           <CopyClipboard content={APIkey ?? ''} />
