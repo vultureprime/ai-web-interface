e40d3fb1fdb5ff23ec0ea5b2f885908b7cb39ccc
ninew089 <132679550+nongnew52@users.noreply.github.com>
2023-11-18 13:30:45 +0700

    feat: update demo

--- next-limit-api-interface/src/app/plan/components/ChatBotDemo.tsx
+++ next-limit-api-interface/src/app/plan/components/ChatBotDemo.tsx
@@ -2,7 +2,7 @@ import ChatWidget, { ChatProps, Role } from '@@/app/components/ChatWidget'
 import FormProvider from '@@/app/components/hook-form/FormProvider'
 import RHFTextField from '@@/app/components/hook-form/RHFTextField'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { useForm, useWatch } from 'react-hook-form'
@@ -14,15 +14,31 @@ export const askScheme = z.object({
   bot: z.string({}).optional(),
 })
 
+const API_BOT = 'http://13.229.123.142:8005'
 export interface IOpenAIForm extends z.infer<typeof askScheme> {}
 
 export default function ChatBotDemo() {
   const [isEdit, setIsEdit] = useState(false)
   const [streamText, setStreamText] = useState('')
 
+  useQuery({
+    queryKey: ['session'],
+    queryFn: async () => {
+      const { data } = await axios.get(`${API_BOT}/session`)
+      return data
+    },
+    enabled: !localStorage?.session,
+    select({ data }) {
+      localStorage.session = data.uuid
+    },
+  })
+
   const { mutateAsync: queryPromt } = useMutation({
     mutationFn: ({ query_str }: { query_str: string }) => {
-      return axios.get(`/query`, { responseType: 'stream' })
+      return axios.post(`${API_BOT}/query`, {
+        message: query_str,
+        uuid: localStorage.session,
+      })
     },
     onError: (error: any) => {
       console.log(error)
@@ -75,7 +91,6 @@ export default function ChatBotDemo() {
   const onSubmit = async (data: IOpenAIForm) => {
     try {
       const id = answer.length
-
       setAnswer((prevState) => [
         ...prevState,
         {
@@ -85,6 +100,7 @@ export default function ChatBotDemo() {
         },
       ])
       const response = await queryPromt({ query_str: data.query })
+      console.log(response)
       if (response) {
         setValue('query', '')
         const streamReader = response.data.getReader()
@@ -121,8 +137,17 @@ export default function ChatBotDemo() {
   return (
     <div id='demo' className='bg-white py-24 sm:py-32'>
       <div className='mx-auto max-w-7xl px-6 lg:px-8'>
+        <div className='mx-auto max-w-2xl text-center'>
+          <h2 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
+            Demo
+          </h2>
+          <p className='mt-2 text-lg leading-8 text-gray-600'>
+            Join us to explore and experience its versatile features. Let&apos;s
+            make learning enjoyable together!
+          </p>
+        </div>
         <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
-          <div className='mt-5 mx-auto flex justify-center flex-col items-center bg-white border border-gray-50 w-full xl:w-[540px] p-8 rounded-3xl'>
+          <div className='mt-10 mx-auto flex justify-center flex-col items-center bg-white  w-full xl:w-[540px] p-8 rounded-3xl border border-blue-500'>
             <div className='flex justify-between items-end w-full gap-x-8 mb-5'>
               {isEdit && (
                 <RHFTextField
