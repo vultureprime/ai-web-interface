92e0d36e80194209cec3126be61282b79ec94c91
ninew089 <132679550+nongnew52@users.noreply.github.com>
2023-12-09 17:42:07 +0700

    init chat

---
+++ next-chat-interface/src/app/components/Header.tsx
@@ -0,0 +1,15 @@
+import { Icon } from '@iconify/react/dist/iconify.js'
+import React from 'react'
+
+const Header: React.FC = () => {
+  return (
+    <div className='w-full flex flex-col bg-white py-8 gap-5 px-5 border-b  border-b-slate-200'>
+      <h1 className='text-center text-xl flex items-center gap-2'>
+        <Icon icon='mingcute:chat-1-fill' width={48} color='#8E8FFA' />
+        Chat
+      </h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ next-chat-interface/src/app/home/components/ChatBotDemo.tsx
@@ -0,0 +1,80 @@
+import ChatWidget, { ChatProps } from '@@/app/components/ChatWidget'
+import FormProvider from '@@/app/components/hook-form/FormProvider'
+import { zodResolver } from '@hookform/resolvers/zod'
+import axios, { AxiosError } from 'axios'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+export const askScheme = z.object({
+  query: z.string().trim().min(1, { message: 'Please enter your message' }),
+  bot: z.string({}).optional(),
+})
+
+export enum ChatType {
+  Basic,
+  WithoutRag,
+}
+
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_API
+
+export interface IOpenAIForm extends z.infer<typeof askScheme> {}
+
+export default function ChatBotDemo() {
+  const [answer, setAnswer] = useState<ChatProps[]>([])
+  const [chatType, setChatType] = useState<ChatType>(ChatType.Basic)
+
+  const methods = useForm<IOpenAIForm>({
+    resolver: zodResolver(askScheme),
+    shouldFocusError: true,
+    defaultValues: {
+      query: '',
+    },
+  })
+
+  const { handleSubmit, setError, setValue } = methods
+
+  const onSubmit = async (data: IOpenAIForm) => {
+    try {
+      const id = answer.length
+      setAnswer((prevState) => [
+        ...prevState,
+        {
+          id: id.toString(),
+          role: 'user',
+          message: data.query,
+          raw: '',
+        },
+      ])
+      setValue('query', '')
+      const { data: result } = await axios.post(
+        `${chatType === ChatType.Basic ? '/chat' : 'chatWithoutRag'}`,
+        {
+          query: data.query,
+        }
+      )
+      setAnswer((prevState) => [
+        ...prevState,
+        {
+          id: (prevState.length + 1).toString(),
+          role: 'ai',
+          message: result.answer,
+          raw: result.answer,
+        },
+      ])
+    } catch (error) {
+      const err = error as AxiosError<{ detail: string }>
+      setError('bot', {
+        message: err?.response?.data?.detail ?? 'Something went wrong',
+      })
+    }
+  }
+
+  return (
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+      <div className='flex justify-center flex-col items-center bg-white mx-auto max-w-7xl h-screen '>
+        <ChatWidget answer={answer} />
+      </div>
+    </FormProvider>
+  )
+}
